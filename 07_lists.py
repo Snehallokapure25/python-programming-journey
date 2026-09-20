86016ecf56137cb6dd8e3a65f11ca631acbad8f1
Snehal Lokapure <snehallokapure25@gmail.com>
2026-09-20 10:17:10 +0530

    Add Python lists example

---
+++ 07_lists.py
@@ -0,0 +1,9 @@
+subjects = ["Python", "Mathematics", "Physics", "Data Science"]
+
+print("My subjects:")
+print(subjects)
+
+print("First subject:", subjects[0])
+print("Last subject:", subjects[-1])
+
+print("Number of subjects:", len(subjects))
