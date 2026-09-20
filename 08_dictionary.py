5600d6c6a1c5aa8adb2c230ea851a50050ad268d
Snehal Lokapure <snehallokapure25@gmail.com>
2026-09-20 10:17:56 +0530

    Add dictionary example

---
+++ 08_dictionary.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "Snehal",
+    "age": 20,
+    "branch": "AI & DS",
+    "college": "Engineering College"
+}
+
+print("Student Information")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Branch:", student["branch"])
+print("College:", student["college"])
